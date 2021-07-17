52b9b27300f4c41eb067723ab9c432e16fd9ab49
Bhanu Pratap Singh <bp.mnnit@gmail.com>
2021-07-17 16:21:11 +0530

    Created a simple blog posts fetching application, using react redux thunk and lodash

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,38 @@
-import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _, { map } from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  // Thunk's first argument is the dispatch function and the second argument is the getState function
+  await dispatch(fetchPosts());
+
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  /* The above two lines of code can be minimised using lodash's chain */
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value(); // Finally executes the above chain
+};
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
 
-  dispatch({ type: 'FETCH_POSTS', payload: response });
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+/* The following is the solution to fetching users using memoise, to minimise repeated calls */
+/* The drawback of this approach is that we can never make a repeated call ever again */
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
